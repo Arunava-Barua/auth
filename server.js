18000b902d53cd343478caa4fa2b8b1214dd56d4
Arunava-Barua <rahulbarua31@gmail.com>
2022-10-08 09:36:47 +0530

    Secured localhost server using self-signed certificate

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const https = require('https');
+const fs = require('fs');
+
+const PORT = 3000;
+
+const app = express();
+
+app.get('/secret', (req, res) => {
+    return res.send('Personal secret key is 42');
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+https.createServer({
+    key: fs.readFileSync('key.pm'),
+    cert: fs.readFileSync('cert.pem')
+}, app).listen(PORT, () => {
+    console.log('Listening on port ' + PORT);
+});

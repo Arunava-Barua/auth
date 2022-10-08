bdd3345c3cff305732b79fdb9082d18b687e7082
Arunava-Barua <rahulbarua31@gmail.com>
2022-10-08 12:14:54 +0530

    Implemented Helmet js

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const https = require('https');
 const fs = require('fs');
+const helmet = require('helmet');
 
 const PORT = 3000;
 
 const app = express();
+
+app.use(helmet());
 
 app.get('/secret', (req, res) => {
     return res.send('Personal secret key is 42');

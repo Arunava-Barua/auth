db8bfa92cefede66ea3bcf8f2bf4da5ec9870c23
Arunava-Barua <rahulbarua31@gmail.com>
2022-10-09 12:47:28 +0530

    Logout functionality done

--- server.js
+++ server.js
@@ -92,7 +92,10 @@ app.get(
 // -----------------------------------------------------------------------------
 
 // GET requests here
-app.get("/auth/logout", (req, res) => {});
+app.get("/auth/logout", (req, res) => {
+  req.logout();  // Removes req.user and clears any logged in session
+  return res.redirect('/');
+});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Personal secret key is 42");
